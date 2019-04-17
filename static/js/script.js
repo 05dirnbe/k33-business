61b91cda8df4aa64e8ef2ef7ab97276a28eb2a52
Michael Dirnberger <mtd@mpi-inf.mpg.de>
2019-04-17 16:24:01 +0200

    Still debugging form submission.

--- static/js/script.js
+++ static/js/script.js
@@ -183,15 +183,20 @@
 		}
     if(grecaptcha && grecaptcha.getResponse().length > 0)
     {
-         //the recaptcha is checked
-         // Do what you want here
-         alert('Well, recaptcha is checked !');
+         // //the recaptcha is checked
+         // // Do what you want here
+         // alert('Well, recaptcha is checked !');
+
+         	$('#captcha').css("border-color", "#666");
     }
     else
     {
-        //The recaptcha is not cheched
-        //You can display an error message here
-        alert('Oops, you have to check the recaptcha !');
+        // //The recaptcha is not cheched
+        // //You can display an error message here
+        // alert('Oops, you have to check the recaptcha !');
+
+        var error = true;
+  			$('#captcha').css("border-color", "#D8000C");
     }
 
 		//now when the validation is done we check if the error variable is false (no errors)
